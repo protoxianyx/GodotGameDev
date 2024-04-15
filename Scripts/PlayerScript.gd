14ecb5715f7a4346addb997d593fd9de233db7b4
Xianyx <pushkar44320@gmail.com>
2024-04-15 15:45:58 +0530

    Animation Maybe?

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -8,6 +8,8 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,6 +21,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
--- rust_bindings/src/lib.rs
+++ rust_bindings/src/lib.rs
@@ -12,7 +12,9 @@ unsafe impl ExtensionLibrary for MyExtension {}
 // #[derive(GodotClass)]
 // #[class(base=Sprite2D)]
 // struct PlayerOne {
+// 	base: Base<Sprite2D>,
+// 	#[var]
 // 	speed: f64,
 // 	angular_speed: f64,
-// 	base: Base<Sprite2D>
+	
 // }
